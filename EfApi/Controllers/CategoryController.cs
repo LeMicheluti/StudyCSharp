323ebe13ee33deff09bfd1b959a4c14ef9b66c76
LeMicheluti <lemicheluti@gmail.com>
2020-08-14 07:31:40 -0300

    EfApi

---
+++ EfApi/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using EfApi.Data;
+using EfApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EfApi.Controllers
+{
+    [ApiController]
+    [Route("v1/categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+        {
+            var categories = await context.Categories.ToListAsync();
+            return categories;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ EfApi/Startup.cs
@@ -0,0 +1,48 @@
+using EfApi.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EfApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddScoped<DataContext, DataContext>();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("EfApi");
+                });
+
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
