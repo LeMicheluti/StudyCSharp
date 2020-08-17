c76cc5186aec4b9b3394326fb2e4aff66001d4db
LeMicheluti <lemicheluti@gmail.com>
2020-08-17 08:07:24 -0300

    Add Entities and SqlContext

---
+++ RestApiModeloDDD/RestApiModeloDDD.Domain/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApiModeloDDD.Domain.Entities
+{
+    public class Product : Base
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ RestApiModeloDDD/RestApiModeloDDD.Infrastructure/Data/SqlContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiModeloDDD.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace RestApiModeloDDD.Infrastructure.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreateAt") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("CreateAt").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("CreateAt").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
