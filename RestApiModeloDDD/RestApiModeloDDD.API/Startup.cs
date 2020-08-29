23c2ec2d463e37674b15fe922b9ab7ab0e85a940
LeMicheluti <lemicheluti@gmail.com>
2020-08-29 10:47:56 -0300

    Add Swagger and IOC

--- RestApiModeloDDD/RestApiModeloDDD.API/Startup.cs
+++ RestApiModeloDDD/RestApiModeloDDD.API/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using RestApiModeloDDD.Infrastructure.CrossCutting.IOC;
 
 namespace RestApiModeloDDD.API
 {
@@ -26,6 +22,15 @@ namespace RestApiModeloDDD.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Model DDD", Version = "v1" });
+            });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ModuleIOC());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +40,12 @@ namespace RestApiModeloDDD.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Model DDD");
+            });
 
             app.UseHttpsRedirection();
 
@@ -49,3 +60,6 @@ namespace RestApiModeloDDD.API
         }
     }
 }
+
+//https://www.youtube.com/watch?v=plS-rf2UIPI
+//2:03:51
